8e10da84e1744c8d5ef83391bf22986491176de4
Daniel Vera <danieelveera@outlook.com>
2023-08-28 17:38:48 -0400

    finished demo

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import ContactForm from '@/Components/ContactForm'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+   <div className='p-4 max-w-3xl mx-auto'>
+    <h1 className='text-3xl font-bold'>Contact Us</h1>
+    <p>Please fill form below</p>
+
+    <ContactForm />
+   </div>
+  )
+}
